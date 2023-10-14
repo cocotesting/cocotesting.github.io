bc67b4a7a53558514e60065234ef1620ee7a8920
cocotesting <masudacoco74@gmail.com>
2023-10-14 19:45:02 +0900

    updated portfolio 2023

---
+++ assets/js/multilang.js
@@ -0,0 +1,71 @@
+    // multilanguage
+    let savedLang = localStorage.getItem('lang')
+
+    $(function() {
+      if(savedLang){
+        selectText(savedLang);
+      } else {
+      savedLang = '日'
+      selectText(savedLang);
+      }
+      
+      $('#JP').click(function() {
+      var lang = $(this).text();
+        localStorage.setItem('lang', lang);
+        selectText(lang);
+        
+      });
+      $('#TC').click(function() {
+      var lang = $(this).text();
+        localStorage.setItem('lang', lang);
+        selectText(lang);
+        
+      });
+      $('#EN').click(function() {
+        var lang = $(this).text();
+        localStorage.setItem('lang', lang);
+        selectText(lang);
+      });
+
+      $('#JP1').click(function() {
+        var lang = $(this).text();
+          localStorage.setItem('lang', lang);
+          selectText(lang);
+          
+        });
+        $('#TC1').click(function() {
+        var lang = $(this).text();
+          localStorage.setItem('lang', lang);
+          selectText(lang);
+          
+        });
+        $('#EN1').click(function() {
+          var lang = $(this).text();
+          localStorage.setItem('lang', lang);
+          selectText(lang);
+        });
+    });
+
+    function selectText(lang){
+          switch (lang) {
+          case '日': 
+            $('.tc').hide();
+            $('.jp').show();
+            $('.en').hide();
+            break;
+          case '中':
+            $('.tc').show();
+            $('.jp').hide();
+            $('.en').hide();
+            break;
+          case 'EN':
+            $('.tc').hide();
+            $('.jp').hide();
+            $('.en').show();
+            break;
+          default:
+            $('.tc').show();
+            $('.jp').hide();
+            $('.en').hide();
+            }
+        }
